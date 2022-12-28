2fd80fff708ae7cc06d181368f1c99aa32cc2bc0
Sketchbuch <sb.design.de@gmail.com>
2022-12-28 09:50:41 +0100

    chore: restructure theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,9 +5,14 @@ const colourOverlay = Color(0xffcce0c1);
 const colourPaper = Color(0xffffffe6);
 const colourPrimary = Color(0xff006633);
 const colourPrimaryTint = Color(0xff004422);
+const colourSecondary = Color.fromARGB(255, 88, 150, 119);
 const colourSplash = Color.fromARGB(32, 0, 51, 0);
 const colourText = Color(0xff003300);
-const colourSecondary = Color.fromARGB(255, 88, 150, 119);
+
+const fontSizeS = 14.0;
+const fontSizeM = 16.0;
+const fontSizeL = 20.0;
+const fontSizeXl = 24.0;
 
 const themeProjectAon = ColorScheme(
   background: colourPaper,
@@ -40,9 +45,9 @@ ThemeData getTheme() {
     ),
     highlightColor: colourSplash,
     inputDecorationTheme: const InputDecorationTheme(
-      helperStyle: TextStyle(fontSize: 14),
-      hintStyle: TextStyle(color: colourText, fontSize: 16),
-      labelStyle: TextStyle(color: colourText, fontSize: 16),
+      helperStyle: TextStyle(fontSize: fontSizeS),
+      hintStyle: TextStyle(color: colourText, fontSize: fontSizeM),
+      labelStyle: TextStyle(color: colourText, fontSize: fontSizeM),
     ),
     radioTheme: RadioThemeData(
       fillColor: MaterialStateColor.resolveWith((states) => colourPrimary),
@@ -52,17 +57,17 @@ ThemeData getTheme() {
     shadowColor: colourPrimary,
     splashColor: colourSplash,
     textTheme: const TextTheme(
-      headline1: TextStyle(color: colourHeadline, fontSize: 24, fontWeight: FontWeight.bold),
-      headline2: TextStyle(color: colourHeadline, fontSize: 20, fontWeight: FontWeight.bold),
-      headline3: TextStyle(color: colourHeadline, fontSize: 20, fontWeight: FontWeight.bold),
-      headline4: TextStyle(color: colourHeadline, fontSize: 20, fontWeight: FontWeight.bold),
-      headline5: TextStyle(color: colourHeadline, fontSize: 20, fontWeight: FontWeight.bold),
-      headline6: TextStyle(color: colourHeadline, fontSize: 20, fontWeight: FontWeight.bold),
-      bodyText1: TextStyle(color: colourPaper, fontSize: 16),
-      bodyText2: TextStyle(color: colourText, fontSize: 16),
-      subtitle1: TextStyle(color: colourText, fontSize: 16),
-      subtitle2: TextStyle(color: colourText, fontSize: 14),
-      button: TextStyle(color: colourPaper, fontSize: 16),
+      headline1: TextStyle(color: colourHeadline, fontSize: fontSizeXl, fontWeight: FontWeight.bold),
+      headline2: TextStyle(color: colourHeadline, fontSize: fontSizeL, fontWeight: FontWeight.bold),
+      headline3: TextStyle(color: colourHeadline, fontSize: fontSizeL, fontWeight: FontWeight.bold),
+      headline4: TextStyle(color: colourHeadline, fontSize: fontSizeL, fontWeight: FontWeight.bold),
+      headline5: TextStyle(color: colourHeadline, fontSize: fontSizeL, fontWeight: FontWeight.bold),
+      headline6: TextStyle(color: colourHeadline, fontSize: fontSizeL, fontWeight: FontWeight.bold),
+      bodyText1: TextStyle(color: colourPaper, fontSize: fontSizeM),
+      bodyText2: TextStyle(color: colourText, fontSize: fontSizeM),
+      subtitle1: TextStyle(color: colourText, fontSize: fontSizeM),
+      subtitle2: TextStyle(color: colourText, fontSize: fontSizeS),
+      button: TextStyle(color: colourPaper, fontSize: fontSizeM),
     ),
     unselectedWidgetColor: colourPrimary,
     useMaterial3: true,
